3261c3c13db9f636db06444338e5fc845d4ac45f
SharonIvy <kendishivy72@gmail.com>
2019-09-26 10:57:43 +0300

    Auto-generated commit

--- src/app/highlight-bold.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightBoldDirective } from './highlight-bold.directive';
-
-describe('HighlightBoldDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightBoldDirective();
-    expect(directive).toBeTruthy();
-  });
-});
